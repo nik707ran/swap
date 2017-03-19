63ee1942d2a515935f76ff696845833ff2b3b6e5
Nikhil Ranjan <nik707ran@gmail.com>
2017-03-19 09:39:09 +0000

    ver 1.0

---
+++ swap.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 31;
+    int y = 44;
+    
+    printf("X is 31\n");
+    printf("Y is 44\n");
+    
+    //swapping
+    
+    printf("X is now %d \n",x);
+    printf("Y is now %d \n",y);
+    
+}
